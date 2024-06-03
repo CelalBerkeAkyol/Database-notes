1a5f1216cef68c1f5c59ec185d3b2887c931c9d1
Celal Berke Akyol <152324363+CelalBerkeAkyol@users.noreply.github.com>
2024-06-03 21:18:28 +0300

    Update join-questions .sql

--- join-questions .sql
+++ join-questions .sql
@@ -13,4 +13,25 @@ group by p.id
 order by total desc
 limit 3;
 -- Hangi kargo şirketine en çok ödeme yapılmış ? 
+USE northwind;
+SELECT  o.shipper_id, s.company, sum(o.shipping_fee) as total
+FROM orders as o
+INNER JOIN shippers as s
+ON o.shipper_id = s.id
+group by s.id
+order by total desc;
+--'2', 'Shipping Company B', '1580.0000'
+--'3', 'Shipping Company C', '338.0000'
+--'1', 'Shipping Company A', '220.0000'
+
 -- En uygun kargo ücreti hangisidir ? 
+SELECT  o.shipper_id, s.company, avg(o.shipping_fee) as ortalama
+FROM orders as o
+INNER JOIN shippers as s
+ON o.shipper_id = s.id
+where o.shipping_fee > 0
+group by s.id
+order by ortalama desc;
+--'2', 'Shipping Company B', '87.77777778'
+--'1', 'Shipping Company A', '36.66666667'
+--'3', 'Shipping Company C', '28.16666667'
