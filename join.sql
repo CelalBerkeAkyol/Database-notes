467937e84c752848647bd5641a8a0f05574a2fe5
Celal Berke Akyol <152324363+CelalBerkeAkyol@users.noreply.github.com>
2024-06-03 18:31:54 +0300

    Update join.sql

--- join.sql
+++ join.sql
@@ -17,3 +17,11 @@ LEFT JOIN orders ON	customers.id = orders.customer_id
 WHERE orders.id is null;;
 -- Cevap olarak dönen sonuçtan id numarsı 19 olan Alexander isimli kullanıcının sipariş oluşturmadığı görülür bu kullanıcıya özel kupon oluşturulup mail atılabilir.
 -- ### '19', 'Alexander', NULL, NULL ### 
+-- RIGHT JOIN (RIGHT OUTER JOIN): Sağ tablodaki tüm kayıtları ve sol tabloyla eşleşen kayıtları döndürür. Sol tabloyla eşleşmeyen kayıtlar için NULL değerler döndürülür.
+SELECT orders.id as order_id, employees.first_name
+FROM orders
+right JOIN employees 
+ON  employees.id = orders.employee_id 
+order by orders.id;
+-- ## NULL, 'Steven' ##  Steaven hiç satış yapmamış 
+--FULL JOIN (FULL OUTER JOIN): Her iki tablodaki tüm kayıtları döndürür. Eşleşen kayıtlar birleştirilir, eşleşmeyenler için NULL değerler döndürülür. (MySQL'de doğrudan desteklenmez, UNION kullanılarak elde edilir.)
