62a9811c36ecb0ff1d128282a7030d65a1e79bfc
Celal Berke Akyol <152324363+CelalBerkeAkyol@users.noreply.github.com>
2024-06-04 20:44:16 +0300

    Create join_questions2.sql

---
+++ join_questions2.sql
@@ -0,0 +1,42 @@
+--# tablolar 
+-- - bolumler 
+-- - dersler
+-- - fakulteler
+-- - notlar
+-- - ogrenciler
+-- Her öğrencinin adı, soyadı, aldığı ders kodu ve vize notlarının listelenmesi
+SELECT o.ograd, o.ogrsoyad, n.derskodu, n.vize 
+FROM ogrenciler o 
+RIGHT JOIN notlar n
+ON o.ogrno = n.ogrno;
+--Vize notu olmayan(girilmeyen) öğrencilerin adları ve soyadlarının listesi
+SELECT * 
+FROM ogrenciler o
+LEFT JOIN notlar n
+on o.ogrno = n.ogrno
+where n.vize is null; 
+-- Vize notu olmayan(girilmeyen) kız öğrenciler kaç kişidir?
+SELECT count(*) as kız_sayısı
+FROM ogrenciler o 
+LEFT JOIN notlar n
+ON o.ogrno = n.ogrno
+where n.vize is null and o.cinsiyet = "bayan";
+--Her bir cinsiyete göre vize notu olmayan(girilmeyen) öğrenci sayılarını bulunuz.
+SELECT o.cinsiyet, count(*)
+FROM ogrenciler o 
+LEFT JOIN notlar n 
+ON o.ogrno = n.ogrno 
+WHERE n.vize is null
+group by o.cinsiyet;
+--Resim bölümündeki öğrencilerin final not ortalamaları kaçtır?
+SELECT avg(final)
+FROM ogrenciler o 
+INNER JOIN notlar n  ON o.ogrno = n.ogrno 
+INNER JOIN bolumler b on o.bolumkod = b.bolumno
+WHERE b.bolumad = "Resim";
+--Dersin adı, o dersteki en yüksek ve ortalama vizeyi listeleyiniz.
+SELECT d.dersadi,avg(vize) as ortalama, max(vize) as en_yuksek
+FROM notlar n  
+INNER JOIN dersler d
+ON n.derskodu = d.derskodu
+group by d.dersadi;
